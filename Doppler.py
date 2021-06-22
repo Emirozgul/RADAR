f21038a8676b0eac9cef3f70b61800ce9c67c69d
Emirozgul <75851346+Emirozgul@users.noreply.github.com>
2021-06-22 10:18:48 +0300

    Update Doppler.py

--- Doppler.py
+++ Doppler.py
@@ -1,16 +1,15 @@
 import math
-import statistics
 import numpy as np
 import matplotlib.pyplot as plt
 
 #Reading File
 import soundfile as sf
-data, Fs = sf.read('Off of Newton Exit 17.wav')
+data, Fs = sf.read('dopdeneme20.wav')
 
 c=3e8
-Tp=0.250 #pulse time
+Tp=0.25 #pulse time
 N=Tp*Fs  #samples per pulse 
-fc=2590e6 #center freq.
+fc=2620e6 #center freq.
 #print(int(N))
 
 def dbc(ar):
@@ -18,10 +17,10 @@ def dbc(ar):
 
 dbc_2=np.vectorize(dbc)
 
+#Be sign is (-), inverted plus sign
 s=-1*data[:,1]
 s_size=np.size(s)
 s_round=round(s_size/N)
-#print(s_round)
 
 #Creating doppler vs. time plot data set here
 for i in range(1,s_round):
@@ -31,30 +30,32 @@ for i in range(1,s_round):
        sif[i-1,:]=s[(i-1)*int(N):i*int(N)]
 
 #Subtraction the average DC term 
-ave=s.mean()
-sif=sif-ave
-#print(sif)
-#print(ave)
+ave=sif.mean(axis=0)
+for i in range(np.size(sif,0)+1):
+    sif[i-1,:]=sif[i-1,:]-ave
+    
+#ave=s.mean() # Second way
+#sif=sif-ave
 
 #Doppler vs. time plot:
-zpad=int(8*N/2)
-v=dbc_2(np.fft.ifft(sif,zpad))
+zpad=int(4*N)
+sif2=sif[1:np.size(sif,0),:]-sif[0:np.size(sif,0)-1,:]  #2-pulse cancellor clutter rejection
+sif3=sif2[:,1:np.size(sif2,1)]-sif2[:,0:np.size(sif2,1)-1]
+v=dbc_2(np.fft.fft(sif2,zpad))
 ss_1=int(np.size(v,1)/2)
 ss=v[:,0:ss_1]
 m=np.amax(ss)
-#print(m)
-#print(ss)
 
 #Calculating velocity
 delta_f=np.linspace(0,Fs/2,np.size(ss,1))
-#print(delta_f)
 lamda=c/fc
 velocity=delta_f*lamda/2
 
 #Time
 time=np.linspace(1,Tp*np.size(ss,0),np.size(ss,0))
 
-plt.imshow(ss-m,cmap='jet',extent=(velocity[0],velocity[np.size(velocity)-1],time[0],time[np.size(time)-1]),aspect='auto')
+#Image part
+plt.imshow(ss-m,cmap='jet',extent=(velocity[0],velocity[np.size(velocity)-1],time[np.size(time)-1],time[0]),aspect='auto')
 plt.xlim(0,30)
 #plt.xticks(np.arange(min(x), max(x)+1, 1.0))
 plt.xlabel('Velocity (m/sec)')
